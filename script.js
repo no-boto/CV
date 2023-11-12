fbc3fc9933d64c41c5f76301a848ab4b712fc89a
Tobo <98432909+no-boto@users.noreply.github.com>
2023-11-12 14:53:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function checkWords() {
 
     for (let i = 0; i < words.length; i++) {
         if (words[i].toUpperCase() !== correctWords[i]) {
-            alert('¡Palabras incorrectas! Intenta de nuevo.');
+            alert('Respuestas incorrectas! Intenta de nuevo.');
             return;
         }
     }
